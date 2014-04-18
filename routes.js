f1b28e7c43ef04c01666a854fb29d938f6c53572
alex mejias <alexdmejias@gmail.com>
2014-04-18 00:59:34 -0400

    mock get + post routes

--- routes.js
+++ routes.js
@@ -1,13 +1,29 @@
-module.exports = function(app) {
+var express = require('express');
+var router = express.Router();
+var bookSchema = require('./schemas/book');
 
-    app.get('/hello/:name', function(req, res) {
-        console.log('hello');
-        res.send('home page ' + req.params.name)
-    });
+router.route('/books')
+    .get(function(req, res) {
+        res.json({status: 'this will be all the books'})
+    })
+    .post(function(req, res) {
+        var book = new bookSchema({
+            slug: 'test_book',
+            name: 'this is the test name',
+            pages: 300,
+            currentPage: 150,
+            Author: 'Some author name'
+        });
 
-    app.get('/about', function(req, res) {
-        res.send('about page')
-    });
+        book.save(function(err) {
+            if (err) {
+                console.log(err);
+                res.status(500).json({status: 'Fail. Could not save'})
+            } else {
+                res.json({status: 'success'})
+            }
+        })
+        res.json({status:'done'})
+    })
 
-
-};
+module.exports = router;
