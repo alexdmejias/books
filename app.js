7b28318104a049568e5eb0ae6b6d7de85ceb4b02
alex mejias <alexdmejias@gmail.com>
2014-04-18 16:47:36 -0400

    removing jade since angular doesn't need it

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ module.exports = function() {
     var bodyParser = require('body-parser');
     var logger = require('morgan');
     var path = require('path');
-    var jade = require('jade');
     var db = require('./db');
 
     var apiRoutes = require('./routes/api');
     var appRoutes = require('./routes/app');
 
-    app.set('view engine', 'jade');
     app.set('views', path.join(__dirname, 'views'));
 
     app.use(logger('dev'));
     app.use(bodyParser());
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'assets')));
 
     app.use('/api', apiRoutes);
     app.use('/', appRoutes);
--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.route('/')
     .get(function(req, res) {
-        res.render('index')
+        res.sendfile('views/index.html')
     });
 
 module.exports = router;
