3dcbcea3091678cfa712435b757fdd43c0cf53fe
alex mejias <alexdmejias@gmail.com>
2014-04-18 00:59:11 -0400

    more app changes (routes, middleware, db, settings)

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 module.exports = function() {
     var express = require('express'); // is it needed?
     var app = express();
-    // var routes = require('./routes');
+    var bodyParser = require('body-parser');
     var logger = require('morgan');
     var path = require('path');
     var jade = require('jade');
+    var router = require('./routes');
+    var db = require('./db');
+
+    app.set('view engine', 'jade');
+    app.set('views', path.join(__dirname, 'views'));
 
     app.use(logger('dev'));
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
+    app.use(bodyParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-    // get all the routes
-    require('./routes')(app);
+    app.use('/api', router);
 
     return app;
 }
